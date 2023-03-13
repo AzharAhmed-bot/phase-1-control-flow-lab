0242ad434ca03f6cbd7d6408dd98e5fded068790
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-03-13 13:36:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if(ride<=400){
+    return'This one is on me!';
+  }
+  else if(ride>2000 && ride<2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+ else if(ride>=2500) {
+  return 'No can do.';
+ }
   // Write your code here!
 }
-
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const cityMessage = city==="NYC" ? 'Ok, sounds good.': 'No go.';
+  return cityMessage;
+ }
+ console.log(ternaryCheckCity("NYC"));
+ function switchOnCharmFromTip(tip){
+  switch(tip){
+      case'generous':
+          return "Thank you so much.";
+          break;
+      case 'not as generous':
+          return "Thank you.";
+          break;
+      default :
+       return "Bye.";
+  }
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  onsole.log(switchOnCharmFromTip("Thank you for everything"));
